c66df66a743f0560995fc56363a72aa5b8cc72ec
MrCris69 <emaildepruebaff123@gmail.com>
2026-07-16 00:15:18 -0500

    Creación de la estructura lógica de la clase BloqueLeer y Bloque Funcion

--- ProjectoDiagramaPseudocodigoP2/src/modulos/datos/BloqueFuncion.java
+++ ProjectoDiagramaPseudocodigoP2/src/modulos/datos/BloqueFuncion.java
@@ -1,5 +1,41 @@
 package modulos.datos;
 
-public class BloqueFuncion {
-    
+import core.modelo.BloqueBase;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BloqueFuncion extends BloqueBase {
+    private String nombreFuncion;
+    private String parametros;
+
+    public BloqueFuncion(int x, int y, String nombreFuncion, String parametros) {
+        super(x, y, 150, 50, nombreFuncion + "(" + parametros + ")");
+        this.nombreFuncion = nombreFuncion;
+        this.parametros = parametros;
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        g.setColor(new Color(255, 200, 150)); // Naranja claro
+        g.fillRect(x, y, ancho, alto);
+        
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, ancho, alto);
+        
+        // Líneas laterales típicas del bloque de función
+        g.drawLine(x + 15, y, x + 15, y + alto);
+        g.drawLine(x + ancho - 15, y, x + ancho - 15, y + alto);
+        
+        g.drawString(texto, x + 25, y + 30);
+    }
+
+    @Override
+    public String generarPseudocodigo() {
+        return "Llamar " + texto; 
+    }
+
+    @Override
+    public String guardarDatosFichero() {
+        return "FUNCION|" + x + "|" + y + "|" + nombreFuncion + "|" + parametros;
+    }
 }
--- ProjectoDiagramaPseudocodigoP2/src/modulos/datos/BloqueLeer.java
+++ ProjectoDiagramaPseudocodigoP2/src/modulos/datos/BloqueLeer.java
@@ -1,5 +1,43 @@
 package modulos.datos;
 
-public class BloqueLeer {
-    
+import core.modelo.BloqueBase;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
+public class BloqueLeer extends BloqueBase {
+    private String variable;
+
+    public BloqueLeer(int x, int y, String variable) {
+        super(x, y, 140, 50, "Leer " + variable);
+        this.variable = variable;
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        // 1. Crear el paralelogramo (inclinado)
+        int[] puntosX = {x + 15, x + ancho, x + ancho - 15, x};
+        int[] puntosY = {y, y, y + alto, y + alto};
+        Polygon paralelogramo = new Polygon(puntosX, puntosY, 4);
+
+        g.setColor(new Color(144, 238, 144)); // Verde claro
+        g.fillPolygon(paralelogramo);
+        
+        g.setColor(Color.BLACK); // Borde
+        g.drawPolygon(paralelogramo);
+        
+        // Texto centrado
+        g.drawString(texto, x + 30, y + 30);
+    }
+
+    @Override
+    public String generarPseudocodigo() {
+        return "Leer " + variable;
+    }
+
+    @Override
+    public String guardarDatosFichero() {
+        
+        return "LEER|" + x + "|" + y + "|" + variable;
+    }
 }
