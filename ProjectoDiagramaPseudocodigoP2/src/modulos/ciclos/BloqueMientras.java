af42b79910b7472af90679b9c90b8659083df600
grleox7 <leandrodick1508@gmail.com>
2026-07-16 14:23:08 -0500

    1er Commit LeandroRam

--- ProjectoDiagramaPseudocodigoP2/src/modulos/ciclos/BloqueMientras.java
+++ ProjectoDiagramaPseudocodigoP2/src/modulos/ciclos/BloqueMientras.java
@@ -1,5 +1,35 @@
 package modulos.ciclos;
+import core.modelo.BloqueBase;
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class BloqueMientras {
+public class BloqueMientras extends BloqueBase {
+    private String condicion;
+
+    public BloqueMientras(int x, int y, String condicion) {
+        super(x, y, 150, 60, "Mientras " + condicion);
+        this.condicion = condicion;
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        g.setColor(new Color(135, 206, 250)); // Azul cielo (Sky Blue)
+        g.fillOval(x, y, ancho, alto); // Óvalo para el ciclo Mientras
+        
+        g.setColor(Color.BLACK);
+        g.drawOval(x, y, ancho, alto);
+        
+        g.drawString(texto, x + 25, y + 35);
+    }
+
+    @Override
+    public String generarPseudocodigo() {
+        return "Mientras " + condicion + " Hacer\n    // Tareas del bucle\nFin Mientras";
+    }
+
+    @Override
+    public String guardarDatosFichero() {
+        return "MIENTRAS|" + x + "|" + y + "|" + condicion;
+    }
     
 }
--- ProjectoDiagramaPseudocodigoP2/src/modulos/ciclos/BloqueRepetir.java
+++ ProjectoDiagramaPseudocodigoP2/src/modulos/ciclos/BloqueRepetir.java
@@ -1,5 +1,35 @@
 package modulos.ciclos;
 
-public class BloqueRepetir {
-    
+import core.modelo.BloqueBase;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BloqueRepetir extends BloqueBase {
+     private String condicionFinal;
+
+    public BloqueRepetir(int x, int y, String condicionFinal) {
+        super(x, y, 160, 50, "Repetir hasta " + condicionFinal);
+        this.condicionFinal = condicionFinal;
+    }
+
+    @Override
+    public void dibujar(Graphics g) {
+        g.setColor(new Color(216, 191, 216)); // Cardo (Thistle/Morado suave)
+        g.fillRoundRect(x, y, ancho, alto, 30, 30); // Bordes muy redondeados
+        
+        g.setColor(Color.BLACK);
+        g.drawRoundRect(x, y, ancho, alto, 30, 30);
+        
+        g.drawString(texto, x + 15, y + 30);
+    }
+
+    @Override
+    public String generarPseudocodigo() {
+        return "Repetir\n    // Tareas\nHasta Que " + condicionFinal;
+    }
+
+    @Override
+    public String guardarDatosFichero() {
+        return "REPETIR|" + x + "|" + y + "|" + condicionFinal;
+    }
 }
