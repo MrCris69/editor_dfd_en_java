26fbf2572c4cac4b654fbeadf7d624370482f2cc
Rax-source <raxgaxsvr@gmail.com>
2026-07-17 13:45:30 -0500

    Gestor para unir pseudocodigo

--- ProjectoDiagramaPseudocodigoP2/src/core/controlador/GestorProyecto.java
+++ ProjectoDiagramaPseudocodigoP2/src/core/controlador/GestorProyecto.java
@@ -1,5 +1,26 @@
 package core.controlador;
 
+import core.modelo.BloqueBase;
+import java.util.List;
+import modulos.datos.TraductorDatos;
+import modulos.control.TraductorControl;
+import modulos.ciclos.TraductorCiclos;
+
 public class GestorProyecto {
     
+    public static String generarCodigoFinal(List<BloqueBase> bloques) {
+        StringBuilder codigo = new StringBuilder();
+        codigo.append("Algoritmo ProyectoFinal_Cris\n\n");
+        
+        for (BloqueBase bloque : bloques) {
+            String linea = TraductorDatos.obtenerPseudocodigo(bloque);
+            if (linea.isEmpty()) linea = TraductorControl.obtenerPseudocodigo(bloque);
+            if (linea.isEmpty()) linea = TraductorCiclos.obtenerPseudocodigo(bloque);
+            
+            codigo.append("    ").append(linea).append("\n");
+        }
+        
+        codigo.append("\nFinAlgoritmo");
+        return codigo.toString();
+    }
 }
